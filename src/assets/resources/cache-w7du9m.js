8eebd718d8c23a49396292fefdc9fe1463ef57d0
youyinnn <youyinnn@gmail.com>
2026-07-13 10:45:08 -0400

    update github pages;

--- src/assets/resources/cache-w7du9m.js
+++ src/assets/resources/cache-w7du9m.js
@@ -1,5 +1,5 @@
 
-    console.log('Loading cache: cache-17qax66.js')
+    console.log('Loading cache: cache-w7du9m.js')
     sessionStorage.setItem('postSeries', "[]");
     sessionStorage.setItem('postMetadata', "[]");
     sessionStorage.setItem('postOrder', "[]");
--- src/assets/resources/resources.js
+++ src/assets/resources/resources.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars */
-var resourcesList = ["cache-17qax66.js","code-theme-css-ef37e5d9.js"];
+var resourcesList = ["cache-w7du9m.js","code-theme-css-ef37e5d9.js"];
 module.exports.list = resourcesList
   
